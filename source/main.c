0a5a0b328590a7978e5020fde40667a0bc0041db
Luofeng Xu <lxu092@ucr.edu>
2020-12-01 11:24:20 -0800

    Initializing repository

---
+++ source/main.c
@@ -0,0 +1,121 @@
+/*	Author: lab
+ *  Partner(s) Name: Luofeng xu
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+
+typedef struct task{
+        int state;
+        unsigned long period;
+        unsigned long elapsedTime;
+        int(*TickFct)(int);
+}task;
+
+task tasks[1];
+const unsigned short tasksNum=1;
+const unsigned long tasksPeriod=100;
+
+volatile unsigned char TimerFlag = 0; 
+unsigned long _avr_timer_M = 1; 
+unsigned long _avr_timer_cntcurr = 0; 
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+void TimerOn() {
+	TCCR1B 	= 0x0B;	
+	OCR1A 	= 125;	
+	TIMSK1 	= 0x02; 
+	TCNT1 = 0;
+	_avr_timer_cntcurr = _avr_timer_M;	
+	SREG |= 0x80;
+}
+
+void TimerOff() {
+	TCCR1B 	= 0x00; 
+}
+
+void TimerISR() {
+	unsigned char i;
+	for(i=0;i<tasksNum;++i){
+		if(tasks[i].elapsedTime>=tasks[i].period){
+			tasks[i].state=tasks[i].TickFct(tasks[i].state);
+			tasks[i].elapsedTime=0;
+		}
+		tasks[i].elapsedTime+=tasksPeriod;
+	}
+}
+ISR(TIMER1_COMPA_vect)
+{
+	_avr_timer_cntcurr--;
+	if (_avr_timer_cntcurr == 0) { 	
+		TimerISR(); 				
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+
+
+
+enum Demo_States {shift};
+int Demo_Tick(int state) {
+
+	// Local Variables
+	static unsigned char pattern = 0x80;	// LED pattern - 0: LED off; 1: LED on
+	static unsigned char row = 0xFE;  	// Row(s) displaying pattern. 
+							// 0: display pattern on row
+							// 1: do NOT display pattern on row
+	// Transitions
+	switch (state) {
+		case shift:	
+			break;
+		default:
+			state = shift;
+			break;
+	}	
+	// Actions
+	switch (state) {
+		case shift:	
+			if (row == 0xEF && pattern == 0x01) { // Reset demo 
+				pattern = 0x80;		    
+				row = 0xFE;
+			} else if (pattern == 0x01) { // Move LED to start of next row
+				pattern = 0x80;
+				row = (row << 1) | 0x01;
+			} else { // Shift LED one spot to the right on current row
+				pattern >>= 1;
+			}
+			break;
+		default:
+			break;
+	}
+	PORTC = pattern;	// Pattern to display
+	PORTD = row;		// Row(s) displaying pattern	
+	return state;
+}
+
+
+int main(void) {
+	DDRC=0xFF;PORTB=0x00;
+	DDRD=0xFF;PORTC=0x00;
+	unsigned char i=0;
+	tasks[i].state=shift;
+	tasks[i].period=100;
+	tasks[i].elapsedTime=0;
+	tasks[i].TickFct=&Demo_Tick;
+	TimerSet(100);
+	TimerOn();
+	while (1) {
+    	}
+	return 1;
+}
